4b4fadfbef5dc94ffa48cfe386fb5846219136bd
infotecquest <infotecquest@gmail.com>
2021-04-27 23:11:20 -0300

    Docker support + misc

---
+++ app01/src/main/java/com/infotecquest/app01/api/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.infotecquest.app01.api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(value= "/helloworld")
+	public String sayHello(@RequestParam(value = "name", defaultValue = " Anonymous")String name){
+			return "Hello World "+name;
+	}
+}
